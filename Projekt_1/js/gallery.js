95a03ddf26372a402227181bdaa667654b18dd52
Łukasz Grabowski <lukasz.grabowski1@protonmail.com>
2024-10-24 12:27:37 +0200

    Add image viewer

--- Projekt_1/js/gallery.js
+++ Projekt_1/js/gallery.js
@@ -1,6 +1,16 @@
 const N = 84;
 const gallery = document.querySelector('.gallery');
+const imageTag = document.querySelector('#image-displayed');
 
+const nextBtn = document.querySelector('#image-next');
+const prevBtn = document.querySelector('#image-prev');
+let activeImage = 0;
+
+function displayImage() {
+	imageTag.src = `img/gallery/Photo${activeImage}.png`;
+}
+
+//Displaying all photos in gallery
 for (let i = 1; i <= 84; i++) {
 	let item = document.createElement('div');
 	item.classList.add('item');
@@ -14,9 +24,27 @@ for (let i = 1; i <= 84; i++) {
 	gallery.appendChild(item);
 }
 
+//Click listeners to all gallery photos
 const items = document.querySelectorAll('.item');
 items.forEach(item => {
 	item.onclick = () => {
 		console.log(item.id);
 	};
+});
+
+// Picture viewer buttons
+nextBtn.addEventListener('click', () => {
+	activeImage += 1;
+	if (activeImage > N) {
+		activeImage = 1;
+	}
+	displayImage();
+});
+
+prevBtn.addEventListener('click', () => {
+	activeImage -= 1;
+	if (activeImage <= 0) {
+		activeImage = N;
+	}
+	displayImage();
 });
