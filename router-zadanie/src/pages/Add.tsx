0a130d42c371484ab2c436df8afc3f10182cb693
Łukasz Grabowski <lukasz.grabowski1@protonmail.com>
2025-01-06 22:07:06 +0100

    Add article

--- router-zadanie/src/pages/Add.tsx
+++ router-zadanie/src/pages/Add.tsx
@@ -1,38 +1,48 @@
+import './Add.css';
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { addArticle } from '../data/articles';
-import { useState } from 'react';
 
 function Add() {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
 	const navigate = useNavigate();
 
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		addArticle(title, content);
+		navigate('/blogs');
+	};
+
 	return (
 		<div>
-			<h1>Add</h1>
-			<input
-				type="text"
-				value={title}
-				onChange={event => {
-					setTitle(event.target.value);
-				}}
-			/>
-			<input
-				type="text"
-				value={content}
-				onChange={event => {
-					setContent(event.target.value);
-				}}
-			/>
-
-			<button
-				onClick={() => {
-					addArticle(title, content);
-					navigate('/blogs');
-				}}
-			>
-				Add Article
-			</button>
+			<h1>Dodaj własny artykuł!</h1>
+			<form onSubmit={handleSubmit}>
+				<div className="input-div">
+					<label htmlFor="title">Tytuł:</label>
+					<input
+						type="text"
+						id="title"
+						value={title}
+						onChange={event => setTitle(event.target.value)}
+						className="add-input"
+						required
+					/>
+				</div>
+				<div className="input-div">
+					<label htmlFor="content">Treść:</label>
+					<textarea
+						id="content"
+						value={content}
+						onChange={event => setContent(event.target.value)}
+						className="add-input"
+						required
+					></textarea>
+				</div>
+				<button type="submit" className="add-button">
+					Dodaj artykuł
+				</button>
+			</form>
 		</div>
 	);
 }
